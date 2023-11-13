0081ce6dfc7f95b47b17ec3d8b612f278adc6f06
Welison Wessling <wwelisonw@gmail.com>
2023-11-13 18:21:49 -0300

    feat: created functionality that takes the first and last day of the month

--- src/Components/MonthButton.tsx
+++ src/Components/MonthButton.tsx
@@ -1,3 +1,5 @@
+import { useDataContext } from "../Contexts/DataContext";
+import { formatDate } from "../utils/formatDate";
 import styles from "./MonthButton.module.css";
 
 function nameOfTheMonth(month: number) {
@@ -9,5 +11,23 @@ function nameOfTheMonth(month: number) {
 }
 
 export const MonthButton = ({ month }: { month: number }) => {
-  return <button className={styles.button}>{nameOfTheMonth(month)}</button>;
+  const { setStart, setFinal } = useDataContext();
+
+  function setMonth(month: number) {
+    const date = new Date();
+
+    date.setMonth(date.getMonth() + month);
+
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+    setStart(formatDate(firstDay));
+    setFinal(formatDate(lastDay));
+  }
+
+  return (
+    <button onClick={() => setMonth(month)} className={styles.button}>
+      {nameOfTheMonth(month)}
+    </button>
+  );
 };
