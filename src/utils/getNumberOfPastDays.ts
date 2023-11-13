9e6869c157d28cdd45d44d014c162e2c9a202710
Welison Wessling <wwelisonw@gmail.com>
2023-11-13 18:17:33 -0300

    fix: adjusted current month

--- src/utils/getNumberOfPastDays.ts
+++ src/utils/getNumberOfPastDays.ts
@@ -5,7 +5,7 @@ export function getNumberOfPastDays(daysPast: number) {
   date.setDate(date.getDate() - daysPast);
 
   const dd = String(date.getDate()).padStart(2, "0");
-  const mm = String(date.getMonth()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0");
   const yyyy = date.getFullYear();
 
   return `${yyyy}-${mm}-${dd}`;
